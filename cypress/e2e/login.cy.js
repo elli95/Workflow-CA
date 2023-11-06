ca927dcd6daf5f0c8eb61b50c43353c4aaf2b377
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-11-06 19:05:19 +0100

    Added login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,19 @@
-describe("End to End test", () => {
-  it("cannot submit the login form with invalid credentials and is shown a message", () => {});
+const email = "unit-test-user@stud.noroff.no";
+const password = "test1234";
+
+describe("Login test", () => {
+  it("should allow a valid user with the correct email/password to login", () => {
+    cy.visit("/");
+    cy.wait(1000);
+
+    cy.get("#registerForm button[type=button]").contains("Login").click();
+    cy.wait(1000);
+
+    cy.get("#loginEmail").type(email, "{enter}");
+    cy.get("#loginPassword").type(password);
+    cy.get("#loginForm button[type=submit]").contains("Login").click();
+    cy.wait(1000);
+
+    cy.location("href").should("include", "view=profile&name");
+  });
 });
