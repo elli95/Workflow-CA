a76142880ff4a8e1bfb872835f28e8301a21a6fc
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-11-10 20:46:01 +0100

    Removed the login process, storageMock and added manual selection of localStorage

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,30 +1,20 @@
-import { login } from "./login.js";
+import localStorageMock from "./LocalStorage.mock.js";
 import { logout } from "./logout.js";
 
 const TEST_USER = {
   email: "unit-test-user@stud.noroff.no",
   password: "test1234",
-  accessToken: "testToken",
 };
 
-const mockFetchSuccess = jest.fn().mockResolvedValue({
-  ok: true,
-  json: jest.fn().mockResolvedValue(TEST_USER),
-});
-
-globalThis.fetch = mockFetchSuccess;
-
-const storageMock = {
-  setItem: jest.fn((key, value) => (localStorage[key] = value.toString())),
-  removeItem: jest.fn((key) => delete localStorage[key]),
-};
-
-globalThis.localStorage = storageMock;
+globalThis.localStorage = localStorageMock;
 
 describe("logout test", () => {
   it("checks that the token/profile from browser storage is gone after logging out", async () => {
-    const data = await login();
-    expect(data).toEqual(TEST_USER);
+    localStorage.setItem("token", "testToken");
+    localStorage.setItem("profile", TEST_USER);
+
+    expect(localStorage.token).toBe("testToken");
+    expect(localStorage.profile).toBe(TEST_USER);
 
     logout();
     expect(localStorage.removeItem).toHaveBeenCalledTimes(2);
