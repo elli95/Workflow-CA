b6559f8f40139b2b5ace3aa2a95e8ce1ee0b11c4
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-11-10 20:41:55 +0100

    Added more checks if localStorage has or doesn't have value

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -7,6 +7,7 @@ describe("Logout test", () => {
     cy.wait(1000);
 
     cy.get("#registerForm button[type=button]").contains("Login").click();
+    cy.getAllLocalStorage().should("be.empty");
     cy.wait(1000);
 
     cy.get("#loginEmail").type(email, "{enter}");
@@ -14,6 +15,7 @@ describe("Logout test", () => {
     cy.get("#loginForm button[type=submit]").contains("Login").click();
     cy.wait(1000);
 
+    cy.getAllLocalStorage().should("not.be.empty");
     cy.location("href").should("include", "view=profile&name");
     cy.get("header button[type=button]").contains("Logout").click();
     cy.wait(1000);
